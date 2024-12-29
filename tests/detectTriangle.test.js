c5e8eb6a43f0d5c535783569ee5fdb113b3ccfca
Septian Dika Maulana <C2318G2774@bangkit.academy>
2024-12-29 15:42:45 +0700

    .

--- tests/detectTriangle.test.js
+++ tests/detectTriangle.test.js
@@ -1,15 +1,23 @@
 const detectTriangle = (sideA, sideB, sideC) => {
-  [sideA, sideB, sideC].forEach((side) => {
-    if (!Number.isInteger(side)) {
-      throw new Error('Sides have to be Integer');
-    }
-    if (side < 1) {
-      throw new Error('Strange Triangle');
-    }
-  });
+  // eslint-disable-next-line no-param-reassign
+  [sideA, sideB, sideC] = [sideA, sideB, sideC]
+    .map((side) => {
+      if (!Number.isInteger(side)) {
+        throw new Error('Sides have to be Integer');
+      }
+      if (side < 1) {
+        throw new Error('Strange Triangle');
+      }
+      return side;
+    })
+    .sort();
 
   if (sideA === sideB && sideA === sideC) {
     return 'Equilateral Triangle';
+  }
+
+  if (sideA === sideB || sideB === sideC) {
+    return 'Isosceles Triangle';
   }
 };
 
@@ -36,5 +44,12 @@ describe('Detect the triangle', () => {
   it('Should detect Equilateral Triangle if all sides are equal', () => {
     expect(detectTriangle(1, 1, 1)).toEqual('Equilateral Triangle');
     expect(detectTriangle(1, 2, 2)).not.toEqual('Equilateral Triangle');
+  });
+
+  it('Should detect Isosceles Triangle if only two sides are equal', () => {
+    expect(detectTriangle(1, 1, 2)).toEqual('Isosceles Triangle');
+    expect(detectTriangle(1, 2, 1)).toEqual('Isosceles Triangle');
+    expect(detectTriangle(1, 2, 2)).toEqual('Isosceles Triangle');
+    expect(detectTriangle(4, 1, 4)).toEqual('Isosceles Triangle');
   });
 });
